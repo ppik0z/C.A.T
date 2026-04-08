2140ca30b150985b118f4a5fd90bca6e2a032482
dangkhai1601 <veal44444444@gmail.com>
2026-04-08 11:48:38 +0700

    feat: jwt-decode

--- chatchoi-vue/src/stores/chat.ts
+++ chatchoi-vue/src/stores/chat.ts
@@ -1,20 +1,32 @@
 import { defineStore } from 'pinia';
 import { socket } from '../socket';
+import { jwtDecode } from 'jwt-decode';
 
 export const useChatStore = defineStore('chat', {
     state: () => ({
         messages: [] as any[],
         currentConversationId: 1, // Tạm thời mặc định phòng 1
+        myId: null as number | null,
         isConnected: false,
     }),
 
     actions: {
+        setIdentity(token: string) {
+            try {
+                const decoded: any = jwtDecode(token);
+                this.myId = decoded.userId; // Lấy userId từ Token
+                console.log("Định danh thành công, ID: ", this.myId);
+            } catch (error) {
+                console.error("Token lỏ!");
+            }
+        },
+
         // Đổ dữ liệu lịch sử vào
         setMessages(msgs: any[]) {
             this.messages = msgs;
         },
 
-        // Thêm 1 tin nhắn mới (real-time hoặc vừa gửi)
+        // Thêm 1 tin nhắn mới 
         pushMessage(msg: any) {
             // Tránh trùng lặp nếu đã nhận từ callback gửi tin
             const exists = this.messages.find(m => m.id === msg.id);
@@ -29,7 +41,7 @@ export const useChatStore = defineStore('chat', {
                 conversationId: this.currentConversationId,
                 content: content
             }, (response: any) => {
-                // Nhận phản hồi "savedMsg" từ Server và cập nhật UI ngay
+                // Nhận phản hồi "savedMsg" từ Server và cập nhật UI 
                 if (response && response.id) {
                     this.pushMessage(response);
                 }
